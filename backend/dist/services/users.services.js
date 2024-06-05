19fa7c6dafaa30e553e7077426461df3f1499856
jonnie254 <johnkamanu254@proton.me>
2024-06-05 16:15:59 +0300

    deleted

--- backend/dist/services/users.services.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import mssql from 'mssql'
-// import {v4} from 'uuid'
-// import bcrypt from 'bcryptjs'
-// import lodash from 'lodash'
-// import {User} from '../interfaces/users'
-// import {sqlConfig} from ''
